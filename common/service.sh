60ecdf6f995b7e1ec16c64d20aa916e4059982f2
Val Mandantes <mandantesv@gmail.com>
2017-04-26 22:52:45 +0800

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -5,3 +5,18 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
+
+set_prop() {
+  setprop $1 $2
+  resetprop $1 $2
+}
+
+## You can edit this if you like ;)
+## More information in the XDA thread
+set_prop dalvik.vm.image-dex2oat-filter interpret-only
+## You can edit this if you like ;)
+
+## Don't touch me :P
+set_prop dalvik.vm.image-dex2oat-filter speed
+set_prop dalvik.vm.dex2oat-filter speed
+## Don't touch me :P
