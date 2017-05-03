5cf9c0ccadf90e72a870796691fa3b5d884d1a99
Val Mandantes <mandantesv@gmail.com>
2017-05-03 09:46:16 +0800

    pm.dexopt.bg-dexopt

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,3 +5,18 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
+grep_prop() {
+  REGEX="s/^$1=//p"
+  shift
+  FILES=$@
+  if [ -z "$FILES" ]; then
+    FILES='/system/build.prop'
+  fi
+  cat $FILES 2>/dev/null | sed -n "$REGEX" | head -n 1
+}
+
+API=`grep_prop ro.build.version.sdk`
+if [ $API -gt 24 ]; then
+  resetprop pm.dexopt.bg-dexopt everything
+fi
